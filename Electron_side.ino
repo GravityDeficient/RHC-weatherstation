9442fcfbaf0ab842cff9beec08804c25f59328ad
Shane Easton <Shane.Easton@gmail.com>
2019-03-02 16:04:47 -0800

    External Watchdog + MAX6675 temperature sensor
    Added code to have pin B0 ping the external watchdog
    Added support for MAX6675 temperature sensor

--- Electron_side.ino
+++ Electron_side.ino
@@ -3,17 +3,29 @@
 // There will also be a Particle Photon that will subscribe to the published data.  The Electron uses the
 // cell phone network, so it's important to keep the data usage very low.  The Photon will be on a wifi somewhere
 // (anywhere really).  It will get the published events, parse the terse data, form the HTTP GET request
-// to update Weather Underground or WeatherFlow (aka iKitesurf), and submit those HTTP GET requests via wifi.
+// to update Weather Underground and/or WeatherFlow (aka iKitesurf), and submit those HTTP GET requests via wifi.
 //
-// Wiring to the wind instrument:
-//  - Anemometer GREEN wire to Electron A1
-//  - Anemometer YELLOW wire to 5V
-//  - Anemometer RED wire to GND
+// Wiring to the wind instrument (Davis Instruments Anemometer and weather vane #6410; about $120 from Amazon):
+//  - Anemometer GREEN wire to Electron A1 (Green goes to the wiper on the pot that measures wind direction)
+//  - Anemometer YELLOW wire to 3.3V  (Red and Yellow are the two ends of the pot that measures direction)
+//  - Anemometer RED wire to GND  (Red and black are the cup switch - but polarity is IMPORTANT)
 //  - Anemometer BLACK wire to Pin 1 of the Schmitt trigger (74HC14)
-//  - Schmitt trigger pin 2 to Electron D2
+//  - Schmitt trigger pin 2 to Electron D2 (interrupt 0)
 //  - Schmitt trigger pin 14 to 5V and pin 7 to GND
 //  - Schmitt trigger pin 1 is pulled up to Vcc with a 10K resistor
 //  - Ceramic "104" cap between Gnd and Schmitt trigger pin 1
+//
+// Wiring to the MAX6675 temperature sensor:
+//  - VCC to Electron 3.3V
+//  - GND to Electron GND
+//  - CS or SS to Electron A2
+//  - SCK to Electron A3
+//  - SO or MISO to Electron A4
+//
+// Wiring the Watchdog/Arduino Pro-Mini
+//  - Electron B0 held high (3.3V) with a 20K resistor
+//  - Electon B0 to Arduino D2.  This is the line that pings the Arduino to let it know all is well still.
+//  - Arduino D3 to Electron Reset.  This resets the Electron if a ping isn't received for 20 minutes.
 //
 // On this anemometer:
 //   Red/Black: cup switch   (Red to GND)
@@ -21,10 +33,17 @@
 //   Green: the wiper on the direction pot (Green to A1)
 //   I currently have the wind vane set up so that it reads 175.5 when it's exactly aligned with the arm
 //     (i.e. arm pointing to the West and wind coming from the west.)
-//   Speed in mph = 2.174 * counts/sec
+//   Speed in mph = 2.174 * counts/sec (for )
 //
 // Solar stuff (looks like this settles into about 43mA at 5V; but frequently goes up to 160mA.
 //   Apparently it can run more than 1A during a publish.)
+//
+// Solar charge controller:
+// - Connect the battery, solar panel, then load.  Dicsonnect them in the reverse order
+// - On the solar charge controller there's a low-voltage cut-off which defaults to 10.7V.  There's also a "reconnect" threshold
+//   which defaults to 12.6V.  I reprogrammed the reconnect to occur at 12.4 volts.
+// - The system draws just under 40mA from the 12V battery once it's up and running.
+//   So that should run 175 hours (about one week) on a 7AH gel cell.
 //
 // To-do:
 // - Figure out why I get a big jump in wind gust when I have a connection outage.
@@ -50,13 +69,18 @@
 // - Figure out something about webhooks, REST, IFTTT
 //
 // rhc 2017-02-19
+// rhc 2018-01-01   Added support for MAX6675 temperature sensor
+// rhc 2018-01-10   Added code to have pin B0 ping the external watchdog
 //===============================================================================================================
 #include <math.h>
+#include <stdlib.h>
 
 #define R2D (57.2958)
 #define WIND_DIR_OFFSET (-94.5)
 #define PUBLISH_FREQ (48)       // We take wind readings every 10 seconds.  We do PUBLISH_FREQ wind readings for each published event
-#define CNTS_TO_MPH (2.174)     // Speed in mph = 2.174 * counts/sec
+
+#define CNTS_TO_MPH (2.174)     // Speed in mph = 2.174 * counts/sec for anemometer used at Mussel Rock (per my own calibration)
+// #define CNTS_TO_MPH (2.25)     // Speed in mph = 2.25 * counts/sec for Davis 6410 Anemometer
 
 volatile int counter;
 int first_event;
@@ -74,20 +98,52 @@ unsigned long last_gust_time_in_secs;
 
 void publish_wind_event(float dt);
 int is_dst(void);
+void click(void);
+
+
+// From Arduino MAX6675 library ==============================
+class MAX6675
+{
+public:
+    MAX6675(int8_t SCLK, int8_t CS, int8_t MISOX);
+
+    double readCelsius(void);
+    double readFahrenheit(void);
+    // For compatibility with older versions:
+    double readFarenheit(void) { return readFahrenheit(); }
+private:
+    int8_t sclk, miso, cs;
+    uint8_t spiread(void);
+};
+
+
+// Define SPI communication pins for the MAX6675 temperature sensor
+int ktcCS  = A2;  // CS or SS - chip-select or slave-select
+int ktcCLK = A3;  // SCK - serial clock
+int ktcSO  = A4;  // MISO - master in - slave out
+
+
+MAX6675 ktc(ktcCLK, ktcCS, ktcSO);  // Temperature sensor
 
 ApplicationWatchdog wd(3600000, System.reset);  // Set the watchdog to reboot the Electron if it doesn't get pinged for 1 hour
 
 
 
+
 void setup()
 {
-    Serial.begin(9600);
-    led = 0;
-    pinMode(4, OUTPUT);  // This will ping the external watchdog
-    pinMode(7, OUTPUT);  // controls LED
-    pinMode(2, INPUT);
     int dst_status;
     int hr, min, sec;
+
+    Serial.begin(9600);
+
+    led = 0;
+    pinMode(B0, OUTPUT);  // This will ping the external watchdog
+    pinMode(D7, OUTPUT);  // controls LED
+    pinMode(D2, INPUT);   // This reads the input from the wind cups
+
+
+    digitalWrite(B0, HIGH);
 
     attachInterrupt(D2, click, RISING);      // Call click() when interrupt 0 (digital pin 2) rises
 
@@ -145,7 +201,7 @@ void loop()
 
     dir = analogRead(1);
 
-    angle = (345.0/4095.0) * (float)(dir) - WIND_DIR_OFFSET;  // NOTE: we use 345 rather than 360 because there's a roughly 15 degree dead-spot
+    angle = (345.0/4095.0) * (float)(dir);  // NOTE: we use 345 rather than 360 because there's a roughly 15 degree dead-spot
 
     x = cos(angle/R2D);
     y = sin(angle/R2D);
@@ -156,7 +212,8 @@ void loop()
 
     //-3- it looks like this is the problem.  If we lose connection the time could end up being much more than 10 seconds.
     dt = (float)(time_in_secs - last_gust_time_in_secs);
-    speed = 2.174 * (float)(wind_cntr - last_wind_cntr) / dt;  // this gives us cup_rotations/second for the current 10-second interval
+    // speed = 2.174 * (float)(wind_cntr - last_wind_cntr) / dt;  // this gives wind speed in mph for the anemometer at Mussel Rock
+    speed = CNTS_TO_MPH * (float)(wind_cntr - last_wind_cntr) / dt;  // this gives wind speed in mph for the Davis 6410 anemometer
     if (speed > gust) gust = speed;
     last_wind_cntr = wind_cntr;
     loop_counter += 1;
@@ -169,12 +226,12 @@ void loop()
 
     if (led_on == 1)
     {
-        digitalWrite(7, LOW);
+        digitalWrite(D7, LOW);
         led_on = 0;
     }
     else
     {
-        digitalWrite(7, HIGH);
+        digitalWrite(D7, HIGH);
         led_on = 1;
     }
 
@@ -182,7 +239,11 @@ void loop()
     if (loop_counter >= PUBLISH_FREQ)
     {
         wd.checkin();
-        // digitalWrite(4, HIGH);  // Trigger the external watchdog to let it know we're still alive
+
+        digitalWrite(B0, LOW);  // Trigger the external watchdog to let it know we're still alive
+        delay(1);
+        digitalWrite(B0, HIGH);
+
         Serial.print("Time in seconds: ");
         dt = (float)(time_in_secs-last_time_in_secs);
         Serial.println(dt);
@@ -210,6 +271,8 @@ void publish_wind_event(float dt)
     int wind_gust;
     int wind_dir;
     float dir;
+    double temperature;
+    int itemp;
 
     // Use global "wind_cntr" as the total cup turns for the publish interval
     // Use global "gust" as the max 10 second cup speed (in rotations/second)
@@ -222,6 +285,11 @@ void publish_wind_event(float dt)
     wind_dir = (int)(dir + 0.5);
     if (wind_dir >= 360) wind_dir -= 360;
     if (wind_dir < 0) wind_dir += 360;
+    /*
+    temperature = ktc.readFahrenheit();  // Mussel Rock sensor doesn't have a temperature sensor
+    itemp = (int)(temperature + 0.5);
+    sprintf(buf, "%02d%02d%03d%03d", wind_speed, wind_gust, wind_dir, itemp);
+    */
     sprintf(buf, "%02d%02d%03d", wind_speed, wind_gust, wind_dir);
     Serial.print("publish: ");
     Serial.println(buf);
@@ -230,6 +298,10 @@ void publish_wind_event(float dt)
     // We give this event a unique name instead of naming it something like "wind" so we don't
     // end up reading everyone elses "wind" events.
     // -3- Ultimately I may publish these as private events so that only I can subscribe to them.
+    /*  These are for Joe's sensor
+    if (first_event) Particle.publish("JL3XX", buf);
+    else             Particle.publish("JL3X", buf);
+    */
     if (first_event) Particle.publish("WL3XX", buf);
     else             Particle.publish("WL3X", buf);
     first_event = 0;
@@ -265,3 +337,83 @@ int is_dst()
     if (imonth == 11 && iday <=5) return(1);
     return(0);
 }
+
+
+
+
+// =========================================================================================
+// From Arduino MAX6675 library    www.ladyada.net/learn/sensors/thermocouple
+
+MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISOX)
+{
+    sclk = SCLK;
+    cs = CS;
+    miso = MISOX;
+
+    //define pin modes
+    pinMode(cs, OUTPUT);
+    pinMode(sclk, OUTPUT);
+    pinMode(miso, INPUT);
+
+    digitalWrite(cs, HIGH);
+}
+
+
+
+double MAX6675::readCelsius(void)
+{
+    uint16_t v;
+
+    digitalWrite(cs, LOW);
+    delay(1);
+
+    v = spiread();
+    v <<= 8;
+    v |= spiread();
+
+    digitalWrite(cs, HIGH);
+
+    if (v & 0x4)
+    {
+        // uh oh, no thermocouple attached!
+        return NAN;
+        //return -100;
+    }
+
+    v >>= 3;
+
+    return v*0.25;
+}
+
+
+
+
+double MAX6675::readFahrenheit(void)
+{
+    return readCelsius() * 9.0/5.0 + 32;
+}
+
+
+
+
+byte MAX6675::spiread(void)
+{
+    int i;
+    byte d = 0;
+
+    for (i=7; i>=0; i--)
+    {
+        digitalWrite(sclk, LOW);
+        delay(1);
+        if (digitalRead(miso))
+        {
+            //set the bit to 0 no matter what
+            d |= (1 << i);
+        }
+        digitalWrite(sclk, HIGH);
+        delay(1);
+    }
+
+    return d;
+}
+
---
+++ watchdog.ino
@@ -0,0 +1,60 @@
+//=========================================================================================================
+// This acts as the external watchdog to reset the Electron if it hasn't pinged the Arduino in 
+// 20 minutes.  It should ping the Arduino every 8 minutes
+//=========================================================================================================
+
+volatile int state = 0;
+int counter = 0;
+int loop_counter;
+
+
+
+void setup()
+   {
+   Serial.begin(9600);  
+
+   pinMode(2, INPUT);
+   pinMode(3, OUTPUT);
+   
+   digitalWrite(3, HIGH);  // Start with pin-3 High.  We take it low for 5 msecs to reset the Electron)
+   
+   attachInterrupt(0, blink, FALLING);  // Call blink() when interrupt 0 (digital pin 2) falls
+  
+   loop_counter = 0;
+   }
+
+
+
+
+void loop()
+   {
+   delay(1000); // 1-second delay for each loop
+  
+   loop_counter += 1;
+
+   if (state == 1)
+      {
+      Serial.println(counter);
+      loop_counter = 0;  // We got pinged by the Electron.  Reset the watchdog timer
+      state = 0;
+      }
+   
+   if (loop_counter > 1200)
+      {
+      digitalWrite(3, LOW);  // We hit the timeout period without the Electron pinging us.  Reset the Electron
+      delay(5);
+      digitalWrite(3, HIGH);
+      loop_counter = 0;
+      }
+   }
+
+
+
+
+void blink()
+  {
+  state = 1;
+  counter++;
+  }
+
+  
